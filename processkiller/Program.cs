c04f511ac3a08b9480e913c37fb465e1af965281
Marcin <marcino451@gmail.com>
2018-06-04 22:55:25 +0200

    working on watching processess, creating events

--- processkiller/Program.cs
+++ processkiller/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
 using System.Text;
@@ -11,6 +12,62 @@ namespace processkiller
 {
     class Program
     {
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+        static void Main(string[] args)
+        {
+            CheckProcess();
+            ManagementEventWatcher startWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+            startWatch.EventArrived += new EventArrivedEventHandler(startWatch_EventArrived);
+            startWatch.Start();
+            ManagementEventWatcher stopWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStopTrace"));
+            stopWatch.EventArrived += new EventArrivedEventHandler(stopWatch_EventArrived);
+            stopWatch.Start();
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+            startWatch.Stop();
+            stopWatch.Stop();
+        }
+
+        static void stopWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine("Process stopped: {0}", e.NewEvent.Properties["ProcessName"].Value);
+
+            //CheckProcess();
+        }
+
+        static void startWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine("Process started: {0}", e.NewEvent.Properties["ProcessName"].Value);
+
+            CheckProcess();
+        }
+
+        private static void CheckProcess()
+        {
+            Dictionary<string, string> UserProcess = new Dictionary<string, string>();
+            Process[] pname = Process.GetProcessesByName("notepad");
+            pname.OrderBy(pnames => pnames.StartTime);
+            foreach (Process process in pname)
+            {
+                try
+                {
+                    if (UserProcess.ContainsKey(GetProcessUser(process))) process.Kill();
+                    else
+                        UserProcess.Add(GetProcessUser(process), process.ProcessName);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+
+
+
         private static string GetProcessUser(Process process)
         {
             IntPtr processHandle = IntPtr.Zero;
@@ -34,53 +91,6 @@ namespace processkiller
             }
         }
 
-        [DllImport("advapi32.dll", SetLastError = true)]
-        private static extern bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool CloseHandle(IntPtr hObject);
 
-        static void Main(string[] args)
-        {
-            Process[] proces = Process.GetProcessesByName("notepad");
-
-            foreach (Process process in proces)
-            {
-                Console.WriteLine(process.ProcessName + " " + Program.GetProcessUser(process));
-            }
-
-            while (true)
-            {
-                Process[] procesy = Process.GetProcessesByName("notepad");
-
-                try
-                {
-                    if (procesy.Length > 1)
-                    {
-                        if (procesy[0].StartTime < procesy[1].StartTime)
-                        {
-                            procesy[1].Kill();
-                            foreach (Process process in proces)
-                            {
-                                Console.WriteLine(process.ProcessName + " " + Program.GetProcessUser(process) + " " + process.Id);
-                            }
-                        }
-                        else
-                        {
-                            procesy[0].Kill();
-                            foreach (Process process in proces)
-                            {
-                                Console.WriteLine(process.ProcessName + " " + Program.GetProcessUser(process) + " " + process.Id);
-                            }
-                        }
-                    }
-                }
-                catch
-                {
-
-                }
-
-            }
-        }
     }
 }
